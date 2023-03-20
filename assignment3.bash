ba1862f6aced244140b38fb35d5faf0b07edf08b
mclarke16 <107970544+mclarke16@users.noreply.github.com>
2023-03-20 15:52:06 -0500

    Created bash/sed files

---
+++ assignment3.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Mickey Clarke
+# CS 3424 Systems Programming
+# Assignment 1, Spring 2023
+
+
+sed -E -f assignment2.sed "$@"
---
+++ assignment3.sed
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Mickey Clarke
+# CS 3424 Systems Programming
+# Assignment 1, Spring 2023
